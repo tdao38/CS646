af76f16c22efc72267996871266dadb1fb549c28
tdao38 <tdao38@massmutual.com>
2020-11-03 16:56:00 -0500

    update explore

--- code/explore.py
+++ code/explore.py
@@ -60,7 +60,7 @@ for i in range(metadata.shape[0]):
     metadata.loc[i, 'processed_abstract'] = processed_text
 
 for i in range(topics.shape[0]):
-    text = topics.loc[i, 'query']
+    text = topics.loc[i, 'narrative']
     # Only proceed with non-na cases
     if pd.isna(text):
         continue
@@ -78,7 +78,7 @@ cosine = cosine_similarity(query_vecs, doc_vecs)
 #index of largest cosine similarity:
 results_all = pd.DataFrame()
 for i in range(topics.shape[0]):
-    idx = np.flip(np.argsort(cosine[i]))[:100]
+    idx = np.flip(np.argsort(cosine[i]))[:1000]
     cosine_scores = pd.Series(cosine[i][idx])
     cosine_docs = metadata.loc[idx]['cord_uid'].reset_index(drop=True)
     results = pd.DataFrame()
@@ -97,12 +97,12 @@ tf_idf = TfidfVectorizer()
 doc_vecs_tf_idf = tf_idf.fit_transform(metadata['processed_abstract'])
 query_vecs_tf_idf = tf_idf.transform(topics['processed_query'])
 
-cosine = cosine_similarity(doc_vecs_tf_idf, query_vecs_tf_idf)
+cosine_tf_idf = cosine_similarity(doc_vecs_tf_idf, query_vecs_tf_idf)
 #index of largest cosine similarity:
 results_all_tf_idf = pd.DataFrame()
 for i in range(topics.shape[0]):
-    idx = np.flip(np.argsort(cosine[i]))[:100]
-    cosine_scores = pd.Series(cosine[i][idx])
+    idx = np.flip(np.argsort(cosine_tf_idf[i]))[:100]
+    cosine_scores = pd.Series(cosine_tf_idf[i][idx])
     cosine_docs = metadata.loc[idx]['cord_uid'].reset_index(drop=True)
     results = pd.DataFrame()
     results['cosine_score'] = cosine_scores
@@ -115,14 +115,30 @@ for i in range(topics.shape[0]):
     else:
         results_all_tf_idf = pd.concat([results_all_tf_idf, results])
 
-# State of the art VSM
-# dl = doc_vecs.sum(axis = 1)
-# df = doc_vecs.sum(axis = 0)
-# avdl = dl.sum()/dl.shape[0]
-# C = dl.shape[0]
-# top = np.log(1 + np.log(1 + doc_vecs.toarray()))
-# bottom = 1-0.5 + 0.5*(dl/avdl)
-#
-# query_vecs[0] * top/bottom * np.log((C+1)/df)
+### State of the art VSM
+dl = doc_vecs.sum(axis = 1)
+df = doc_vecs.sum(axis = 0)
+avdl = dl.sum()/dl.shape[0]
+C = dl.shape[0]
 
+for i in range(topics.shape[0]):
+    print(i)
+    topic_ids = []
+    doc_ids = []
+    scores = []
+    for j in range(doc_vecs.shape[0]):
+        print(j)
+        top = np.log(1 + np.log(1 + doc_vecs[j].toarray()))
+        bottom = 1-0.5 + 0.5*(dl[j]/avdl)
+
+        prod = query_vecs[i].toarray() * np.ravel(top/bottom[0]) * np.ravel(np.log((C+1)/df))
+        score = np.sum(prod)
+
+        if score > 0:
+            topic_ids.append(topics.loc[i, 'topic-id'])
+            doc_ids.append(metadata.loc[j, 'cord_uid'])
+            scores.append(score)
+
+
+results_vsm_all = pd.DataFrame()
 # BM25
